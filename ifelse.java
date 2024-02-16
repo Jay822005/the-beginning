46e55aef6f67455a0e44752a2e5a8070cc85f82c
Shreymehta2023 <shreymehta225@gmail.com>
2024-02-16 14:43:08 +0530

    if-else statements

---
+++ ifelse.java
@@ -0,0 +1,14 @@
+/*if the input value is odd or even. If it’s odd print “Odd”, otherwise
+print “Even”.Note: Input value will be between & and &0^6.*/
+public class ifelse {
+    public static void main(String[] args) {
+        int value = 10;
+        if(value%2 == 0) {
+            System.out.println("value "+value+" is even");
+        }
+        else {
+            System.out.println("value "+value+" is odd");
+        }
+    }
+    
+}
