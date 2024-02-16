35f0261f149cf330596be017bb693db9fb781069
Shreymehta2023 <shreymehta225@gmail.com>
2024-02-16 12:06:41 +0530

    Logical AND operator

---
+++ ConditionalOp.java
@@ -0,0 +1,10 @@
+/*Write a program to print the value of input 
+if it is even and divisible by 3.*/
+public class ConditionalOp {
+    public static void main(String[] args){
+    int value  = 6;
+    if(value%2==0 && value%3==0) {
+        System.out.println("value is " + value);
+    }
+}
+}
