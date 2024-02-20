916cd2c5ca60fd239779d5c0acd9c45fbdbad91d
Shreymehta2023 <shreymehta225@gmail.com>
2024-02-20 14:05:51 +0530

    array example

---
+++ Ars6.java
@@ -0,0 +1,15 @@
+public class Ars6 {
+    public static void main(String[] args) {
+        int nums[][]={{1,4,5,7}
+                     ,{5,6,3,8},
+                      {1,0,0,3}};
+
+        for(int i=0;i<3;i++) {
+            for(int j=0;j<4;j++) {
+                System.out.print(nums[i][j]);
+            }
+            System.out.println();
+        }              
+    }
+    
+}
