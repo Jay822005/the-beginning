0a45b85175dfdec301356da907674c25b8bd25d2
Shreymehta2023 <shreymehta225@gmail.com>
2024-02-20 20:19:07 +0530

    ArrayIndexOutofBoundExeption

---
+++ Ars12.java
@@ -0,0 +1,9 @@
+public class Ars12 {
+    public static void main(String[] args) {
+        int x[]={22,4,7,5};
+        System.out.println(x[4]);
+
+        System.out.println("Hello");
+    }
+    
+}
