8e83e29fc47b44a18e956cd9dffb9dedaf75e60b
Shreymehta2023 <shreymehta225@gmail.com>
2024-02-20 11:51:35 +0530

    printing in array

--- Ars2.java
+++ Ars2.java
@@ -1,8 +1,14 @@
 public class Ars2 {
     public static void main(String[] args) {
         int nums[] = {2,4,1,5};
+        String names[] = {"shrey","yash","sahil"};
         for(int i=0;i<4;i++) {
             System.out.println(nums[i]);
+        }
+        int j=0;
+        while(j<3) {
+            System.out.println(names[j]);
+            j++;
         }
     }
     
