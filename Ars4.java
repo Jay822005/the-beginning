dfad6c5d007d98feb4511e51d9f6079e1492ff1d
Shreymehta2023 <shreymehta225@gmail.com>
2024-02-20 12:52:30 +0530

    2d array

---
+++ Ars4.java
@@ -0,0 +1,15 @@
+public class Ars4 {
+    public static void main(String[] args) {
+        int nums[][]={ {1,2},
+                       {3,4},
+                       {8,6} };
+
+            for(int i=0;i<3;i++) {
+                for(int j=0;j<2;j++) {
+                    System.out.print(nums[i][j]);
+                }
+                System.out.println();
+            }            
+    }
+    
+}
