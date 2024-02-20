17fd3353ef22c2beda0040d6ff061fcc6160a6e1
Shreymehta2023 <shreymehta225@gmail.com>
2024-02-20 20:26:11 +0530

    Ars12 solution

---
+++ Ars13.java
@@ -0,0 +1,8 @@
+public class Ars13 {
+    public static void main(String[] args) {
+        int x[] = {5,73,44,6};
+        System.out.println(x[x.length-1]);
+        System.out.println("Hi");
+    }
+    
+}
