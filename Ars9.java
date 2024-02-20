cf0366c371e9c48a50312e3251f370abc776c731
Shreymehta2023 <shreymehta225@gmail.com>
2024-02-20 18:25:44 +0530

    length and length()

---
+++ Ars9.java
@@ -0,0 +1,19 @@
+public class Ars9 {
+    
+    public static void main(String[] args) {
+            int a[]= {2,6,8,3};
+            System.out.println(a.length);
+
+            int b[][]={{3,6,0,3},{2,0,6},{1,0,5}};
+            System.out.println(b.length);
+            System.out.println(b[1].length);
+
+            String name = "shrey";
+            System.out.println(name.length());
+
+            String names[] = {"mehta","shreys"};
+            System.out.println(names.length);
+            System.out.println(names[1].length());
+    }
+
+}
