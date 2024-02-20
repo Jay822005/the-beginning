5e3fd3881e65906872476457e10e13ee92c8c648
Shreymehta2023 <shreymehta225@gmail.com>
2024-02-20 14:16:51 +0530

    for Each loop

---
+++ Ars7.java
@@ -0,0 +1,9 @@
+public class Ars7 {
+    public static void main(String[] args) {
+        int nums[]={5,0,2,5,8,9};
+        for(int n:nums) {
+            System.out.print(n+" ");
+        }
+    }
+    
+}
