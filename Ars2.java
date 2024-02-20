809afae3a52dd4442f8c47022807ecc91dd6d5fb
Shreymehta2023 <shreymehta225@gmail.com>
2024-02-20 11:32:11 +0530

    print aray using loop

---
+++ Ars2.java
@@ -0,0 +1,9 @@
+public class Ars2 {
+    public static void main(String[] args) {
+        int nums[] = {2,4,1,5};
+        for(int i=0;i<4;i++) {
+            System.out.println(nums[i]);
+        }
+    }
+    
+}
