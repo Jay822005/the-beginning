83ce2fb6709188c776a5ccf57dc376b23aecc70c
Shreymehta2023 <shreymehta225@gmail.com>
2024-02-20 14:42:08 +0530

    enhanced for or for each loop

---
+++ Ars8.java
@@ -0,0 +1,12 @@
+public class Ars8 {
+    public static void main(String[] args) {
+        int nums[][]={{3,5,3,2},{6,8,3,1,3},{1}};
+        for(int n[]:nums) {
+            for(int b:n) {
+                System.out.print(b);
+            }
+            System.out.println();
+        }
+    }
+    
+}
