5eeabfa7cc019fea7bac70341617b5ff0c14c92c
Shreymehta2023 <shreymehta225@gmail.com>
2024-02-20 11:15:53 +0530

    aray declaration

---
+++ Ars.java
@@ -0,0 +1,11 @@
+public class Ars {
+    public static void main(String[] args) {
+        int nums[] ={1,4,2,6};
+        System.out.println(nums);
+        System.out.println(nums[0]);
+        System.out.println(nums[1]);
+        System.out.println(nums[2]);
+        System.out.println(nums[3]);
+    }
+    
+}
