9c19d3132d1cce4102ea0b57459f64c37e0c2cad
Shreymehta2023 <shreymehta225@gmail.com>
2024-02-21 20:01:07 +0530

    String declaration

---
+++ Stg1.java
@@ -0,0 +1,18 @@
+public class Stg1 {
+    public static void main(String[] args) {
+        String name = "shrey";
+        System.out.println(name);
+
+        String names = new String("shreys");
+        System.out.println(names);
+
+        char ch[]={'s','h','r','e','y'};
+        String s1 = new String(ch);
+        System.out.println(s1);
+
+        byte b[]={100,44,65};
+        String s2 = new String(b);
+        System.out.println(s2);
+    }
+    
+}
