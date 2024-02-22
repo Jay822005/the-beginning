21e0c81aba8245432b132b76ffeeae8cb0f7ee93
Shreymehta2023 <shreymehta225@gmail.com>
2024-02-22 16:44:06 +0530

    Palindrome or not

---
+++ Stg9.java
@@ -0,0 +1,17 @@
+public class Stg9 {
+    public static void main(String[] args) {
+        String str1="madam";
+        String str2="";
+
+        for(int i=str1.length()-1;i>=0;i--) {
+            str2=str2+str1.charAt(i);
+        }
+        if(str1.equals(str2)) {
+            System.out.println("String is palindrome");
+        }
+        else {
+            System.out.println("String is not palindrome");
+        }
+    }
+    
+}
